aff6b3ed0652b2506442c5dde6b6b6913494166c
Jared Jennings <jared@jaredjennings.org>
2015-10-20 16:45:15 -0500

    fix bug with create_by

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @rule_session = RuleModel.find_or_create_by(:session_id => request.session_options[:id])
+    @rule_session = RuleModel.find_or_create_by!(:session_id => request.session_options[:id])
   end
 
 end
