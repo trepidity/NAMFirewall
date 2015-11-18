10c9c29e694e033557b7fabdcf6547b10e5570fb
Jared Jennings <jared@jaredjennings.org>
2015-11-18 07:34:49 -0600

    Share without any match error

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -1,7 +1,11 @@
 class ShareController < ApplicationController
 
   def show
-    @rule_model = RuleModel.find_by_session_id(params[:id])
+    @rule_model = RuleModel.find_by!(:session_id => params[:id])
+  rescue
+    respond_to do |format|
+      format.html { render :partial => 'share/no_rules_found' }
+    end
   end
 
 end
